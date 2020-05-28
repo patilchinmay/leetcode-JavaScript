da35c125451886071029fb26bcb87a68fed3611f
Chinmay <patilchinmay01@gmail.com>
2020-05-28 12:37:26 +0530

    decode-ways

---
+++ python/Dynamic Programming/decode-ways.py
@@ -0,0 +1,41 @@
+# https://leetcode.com/problems/decode-ways/submissions/
+
+class Solution:
+    def numDecodings(self, s: str) -> int:
+        
+        if not s or s[0] == "0":
+            return 0
+        
+        dp = [1] + [0] * len(s)
+        dp[1] = 1
+        
+        for i in range(2, len(s)+1):
+            # print("START i = ", i, " char = ", s[i-1], " dp = ", dp) 
+
+            if 1 <= int(s[i-1]) <= 9:
+                dp[i] = dp[i-1]
+            
+            if 10 <= int(s[i-2:i]) <= 26:
+                dp[i] += dp[i-2]
+                
+            # print("END i = ", i, " char = ", int(s[i-2:i]), " dp = ", dp) 
+        # print(dp) 
+        return dp[-1]
+
+# "001"
+# "110"
+# "100"
+# "100001"
+# "000"
+# "10101"
+# "1010"
+# ""
+# "0"
+# "1"
+# "99"
+# "226"
+# "2626"
+# "262622"
+# "12222"
+# "122221"
+# "122229"
