2ea6e266c44941a9d0261f495a158e13ab5a87bb
Chinmay <patilchinmay01@gmail.com>
2020-06-12 19:45:22 +0530

    letter-tile-possibilities

--- python/Backtracking/letter-tile-possibilities.py
+++ python/Backtracking/letter-tile-possibilities.py
@@ -5,22 +5,27 @@ class Solution:
         # Using set so that duplicates will not be counted even if input has repeating letters.
         # Use list if it is confirmed that input will have distinct letters.
         answer = set()
-        
-        self.backtrack(0, answer, [], list(tiles) , tiles )
+		
+		# Initially available_items == tiles (as we have not started, so all possibilities are available) and current_path is empty
+        current_path = []
+        available_items = list(tiles) # Using as list so it is easy to manipulate
+		
+        self.backtrack(0, answer, current_path, available_items, tiles )
         
         return(len(answer))
         
     def backtrack(self, index, answer, current_path, available_items, tiles):
-        
+        # define returning condition for recursion (dfs)
         if index == len(tiles):
             return
-        
+			
+        # Initially available_items == tiles (as we have not started, so all possibilities are available) and current_path is empty
         for i in range(len(available_items)):
             '''
             Remove the i'th item from available items
             Add that item to current path
             
-            If a string given in input is "ABC"
+            e.g. If a string given in input is "ABC"
             At index = 0 (before processing next line, the vars will be)
               i=0 current_path = [], available_items = [A, B, C]
               i=1 current_path = [], available_items = [A, B, C]
@@ -31,11 +36,11 @@ class Solution:
               i=1 current_path = [B], available_items = [A, C]
               i=2 current_path = [C], available_items = [A, B]
             
-            At index = 1 (for i=0 from above) (after processing next line, the vars will be)
+            At index = 1 (w.r.t. i=0 from above) (after processing next line, the vars will be)
               i=0 current_path = [A, B], available_items = [C]
               i=1 current_path = [A, C], available_items = [B]
             
-            At index = 2 (for i=0 from above) (after processing next line, the vars will be)
+            At index = 2 (w.r.t. i=0 from above) (after processing next line, the vars will be)
               i=0 current_path = [A, B, C], available_items = []
             
             So on.... 
@@ -46,7 +51,6 @@ class Solution:
             answer.add("".join(current_path))
             
             self.backtrack( index+1, answer, current_path, available_items, tiles )
-
+			
             # Reverse the processing that we did earlier a.k.a. backtrack
             available_items.insert(i, current_path.pop())
-            
