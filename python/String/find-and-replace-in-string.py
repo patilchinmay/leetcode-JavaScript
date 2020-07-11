6798fea4654c00af33eb7d2a974580af47b8238d
Chinmay <patilchinmay01@gmail.com>
2020-07-11 22:08:51 +0530

    find-and-replace-in-string

--- python/String/find-and-replace-in-string.py
+++ python/String/find-and-replace-in-string.py
@@ -4,9 +4,11 @@ class Solution:
     def findReplaceString(self, S: str, indexes: [int], sources: [str], targets: 
 		[str]) -> str:
         
+		# Corner case
         if len(indexes) == 0:
             return S
         
+		# List are mutable and faster to operate with compared to strings
         ans = []
         
         i = 0
@@ -17,19 +19,24 @@ class Solution:
                 # print(i, "not in indexes")
                 ans += S[i]
             else:
+				# Find at which index is 'i' present in indexes so that value can be used to get the relevant sources and targets values.
                 index = indexes.index(i)
                 # print(i, "IS in indexes at index = ", index)
                 
                 # print("\tFinding ", sources[index])
-                
+
+                # Equivalent to => S[i:].startswith(sources[index])
+				# Find if the string has sources[index] at postion i thorugh i+len(sources[index])
                 position = S.find(sources[index], i, i+len(sources[index]))
                 # print("\tPostion = ", position)
                 
                 if position == -1:
+					# sources[index] was not found at the required position
                     ans += [S[i]]
                 else:
+					# # sources[index] was found at the required position. Add targets[index] to answer
                     ans += list(targets[index])
-                    i = i + len(sources[index]) - 1
+                    i = i + len(sources[index]) - 1 # subtracting 1 from correct value as we will add that 1 at the end of the loop (on next line) anyway.
             i+=1    
             # print(ans)
             
