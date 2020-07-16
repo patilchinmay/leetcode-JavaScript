32d1e6591bdde3efbb219d674b0a9fd6daa86cb4
Chinmay <patilchinmay01@gmail.com>
2020-07-16 20:27:13 +0530

    reach-a-number

---
+++ python/Math/reach-a-number.py
@@ -0,0 +1,16 @@
+# https://leetcode.com/problems/reach-a-number/submissions/
+
+class Solution:
+    def reachNumber(self, target: int) -> int:
+        
+        target = abs(target)
+        
+        k  = 0
+        
+        while target > 0:
+            k += 1
+            target -= k
+            
+        # print(target, k)
+        
+        return k if target % 2 == 0 else k + 1 + k%2
